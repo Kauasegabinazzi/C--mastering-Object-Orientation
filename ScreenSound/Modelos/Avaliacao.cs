48f44ca0dc2e5cb561a0ec42db175421e5a01a33
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-20 13:47:47 -0300

    9-commit

--- ScreenSound/Modelos/Avaliacao.cs
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -5,6 +5,9 @@ namespace ScreenSound.Modelos
     {
         public Avaliacao(int nota)
         {
+            if (nota <= 0) nota = 0;
+            if (nota >= 10) nota = 10;
+
             Nota = nota;
         }
 
