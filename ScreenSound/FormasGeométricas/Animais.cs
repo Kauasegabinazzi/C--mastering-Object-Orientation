cf2b39c6345d17ea6c01719094a97daf18aad176
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-25 19:41:51 -0300

    23-commit

---
+++ ScreenSound/FormasGeométricas/Animais.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.FormasGeométricas
+{
+    class Animal
+    {
+        public virtual string Sound()
+        {
+            return "AU";
+        }
+    }
+
+    class Mamifero : Animal
+    {
+        public override string Sound()
+        {
+            return "AU";
+        }
+    }
+
+    class Ave : Animal
+    {
+        public override string Sound()
+        {
+            return "AU";
+        }
+    }
+
+    class Peixe : Animal
+    {
+        public override string Sound()
+        {
+            return "AU";
+        }
+    }
+}
---
+++ ScreenSound/FormasGeométricas/ProdutosEletrônicos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.FormasGeométricas
+{
+    class ProdutoEletronico
+    {
+        public string ?Model { get; set; }
+        public double Price { get; set; }
+
+        public virtual string ShowInformation()
+        {
+            return $"Model: {Model}, Price: {Price:C}";
+        }
+    }
+
+    class Smartphone : ProdutoEletronico
+    {
+        public string ?OperationalSystem { get; set; }
+
+        public override string ShowInformation()
+        {
+            return $"{base.ShowInformation()}, SO: {OperationalSystem}";
+        }
+    }
+
+    class Tablet : ProdutoEletronico
+    {
+        public string ?TypeScreen { get; set; }
+
+        public override string ShowInformation()
+        {
+            return $"{base.ShowInformation()}, Screen: {TypeScreen}";
+        }
+    }
+
+    class Laptop : ProdutoEletronico
+    {
+        public string ?Processor { get; set; }
+
+        public override string ShowInformation()
+        {
+            return $"{base.ShowInformation()}, Processor: {Processor}";
+        }
+    }
+}
