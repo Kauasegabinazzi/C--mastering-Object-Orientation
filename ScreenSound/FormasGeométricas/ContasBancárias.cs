dc6a7cfae111106d0441fa3e1f452b6a58af5529
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-25 19:35:07 -0300

    22-commit

---
+++ ScreenSound/FormasGeométricas/ContasBancárias.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.FormasGeométricas
+{
+    class ContaBancaria
+    {
+        protected double Balance { get; set; }
+
+        public virtual void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public virtual void Withdraw(double value)
+        {
+            Balance -= value;
+        }
+
+        public virtual double CalculateBalance()
+        {
+            return Balance;
+        }
+    }
+
+    class ContaCorrente : ContaBancaria
+    {
+        private double MaintenanceFee { get; set; }
+
+        public override void Withdraw(double value)
+        {
+            base.Withdraw(value + MaintenanceFee);
+        }
+    }
+
+    class ContaPoupanca : ContaBancaria
+    {
+        private double RateIncome { get; set; }
+
+        public override double CalculateBalance()
+        {
+            return base.CalculateBalance() * (1 + RateIncome);
+        }
+    }
+}
---
+++ ScreenSound/FormasGeométricas/Funcionários.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.FormasGeométricas
+{
+    class Funcionario
+    {
+        public string ?Name { get; set; }
+        public double Salary { get; set; }
+    }
+
+    class Gerente : Funcionario
+    {
+        public string ?Sector { get; set; }
+    }
+
+    class Programador : Funcionario
+    {
+        public string ? ProgrammingLanguage { get; set; }
+    }
+
+    class Analista : Funcionario
+    {
+        public string ? OccupationArea { get; set; }
+    }
+}
