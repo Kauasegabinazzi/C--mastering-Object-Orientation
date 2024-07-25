9988590d8d749c83cad6617aaae704f0aa09c313
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-25 19:00:24 -0300

    18-commit

---
+++ ScreenSound/Menus/MenuSaircs.cs
@@ -0,0 +1,17 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Menus
+{
+    internal class MenuSair : Menu
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Tchau tchau :)");
+        }
+    }
+}
