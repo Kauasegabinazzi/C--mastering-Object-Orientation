731a390e5b018151634ebc15c594df627586f1ec
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-27 18:33:12 -0300

    31-commit

---
+++ ScreenSound/Interfaces/INotificavel.cs
@@ -0,0 +1,27 @@
+﻿namespace ScreenSound.Interfaces;
+
+public interface INotificavel
+{
+    void SendNotification();
+}
+
+public class Email : INotificavel
+{
+    public string ?EmailAddress { get; set; }
+
+    public void SendNotification()
+    {
+        Console.WriteLine($"Sending email to {EmailAddress}");
+    }
+}
+
+public class SMS : INotificavel
+{
+    public string ?PhoneNumber { get; set; }
+
+    public void SendNotification()
+    {
+        Console.WriteLine($"Sending SMS to {PhoneNumber}");
+    }
+}
+
---
+++ ScreenSound/Interfaces/IPagavel.cs
@@ -0,0 +1,28 @@
+﻿namespace ScreenSound.Interfaces;
+
+public interface IPagavel
+{
+    decimal CalculatePayment();
+}
+
+public class Produto : IPagavel
+{
+    public decimal UnitPrice { get; set; }
+    public int Amount { get; set; }
+
+    public decimal CalculatePayment()
+    {
+        return UnitPrice * Amount;
+    }
+}
+
+public class Servico : IPagavel
+{
+    public decimal HourlyRate { get; set; }
+    public int WorkedHours { get; set; }
+
+    public decimal CalculatePayment()
+    {
+        return HourlyRate * WorkedHours;
+    }
+}
