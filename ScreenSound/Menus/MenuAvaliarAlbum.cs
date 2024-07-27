f6a8e3e67804c8d299b666b5cf6aa6a85b69c643
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-26 23:29:48 -0300

    26-commit

--- ScreenSound/Menus/MenuAvaliarAlbum.cs
+++ ScreenSound/Menus/MenuAvaliarAlbum.cs
@@ -23,14 +23,22 @@ internal class MenuAvaliarAlbum : Menu
             Console.Write("Agora digite o título do álbum: ");
             string tituloAlbum = Console.ReadLine()!;
 
-            if(banda.Albuns.Count > 0)
+            if(banda.Albuns.Any(a => a.Nome.Equals(tituloAlbum)))
             {
-                Console.Write($"Qual a nota que a banda {tituloAlbum} merece: ");
+                Album album = banda.Albuns.First(a => a.Nome.Equals(tituloAlbum));
+                Console.Write($"Qual a nota que do album {tituloAlbum} merece: ");
                 Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
-                Album album = 
-                banda.AdicionarNota(nota);
+
+                album.AdicionarNota(nota);
                 Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {tituloAlbum}");
                 Thread.Sleep(2000);
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine($"\no album {tituloAlbum} não foi encontrada!");
+                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
                 Console.Clear();
             }
         }
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -17,7 +17,8 @@ opcoes.Add(1, new MenuRegistrarBanda());
 opcoes.Add(2, new MenuRegistrarAlbum());
 opcoes.Add(3, new MenuMostrarBandasRegistradas());
 opcoes.Add(4, new AvaliarBanda());
-opcoes.Add(5, new MenuExibirDetalhes());
+opcoes.Add(5, new MenuAvaliarAlbum());
+opcoes.Add(6, new MenuExibirDetalhes());
 opcoes.Add(-1, new MenuSair());
 
 void ExibirLogo()
@@ -41,7 +42,8 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
     Console.WriteLine("Digite 3 para mostrar todas as bandas");
     Console.WriteLine("Digite 4 para avaliar uma banda");
-    Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+    Console.WriteLine("Digite 5 para avaliar um album");
+    Console.WriteLine("Digite 6 para exibir os detalhes de uma banda");
     Console.WriteLine("Digite -1 para sair");
 
     Console.Write("\nDigite a sua opção: ");
