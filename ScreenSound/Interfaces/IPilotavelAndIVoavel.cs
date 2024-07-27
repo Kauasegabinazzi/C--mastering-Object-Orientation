2f478af5fbc4814ed35c8dc68976ad8a44491591
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-27 18:24:25 -0300

    30-commit

---
+++ ScreenSound/Interfaces/IPilotavelAndIVoavel.cs
@@ -0,0 +1,24 @@
+﻿namespace ScreenSound.Interfaces;
+
+public interface IPilotavel
+{
+    void Pilot();
+}
+
+public interface IVoavel
+{
+    void Fly();
+}
+
+public class Vehicle : IPilotavel, IVoavel
+{
+    public void Pilot()
+    {
+        Console.WriteLine("Vehicle is driving");
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("Vehicle is flying");
+    }
+}
