efae1a04dca2b55d190bd81d561f0850a7476758
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-27 18:17:35 -0300

    29-commit

---
+++ ScreenSound/Interfaces/IForma.cs
@@ -0,0 +1,38 @@
+﻿namespace ScreenSound.Interfaces;
+
+internal interface IForma
+{
+    double CalculateArea();
+    double CalculatePerimeter();
+}
+
+public class Circle : IForma
+{
+    public double Ray { get; set; }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * Math.PI * Ray; 
+    }
+
+    public double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Ray, 2);
+    }
+}
+
+public class Rectangle : IForma
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * (Length + Width);
+    }
+
+    public double CalculateArea()
+    {
+        return Length * Width;
+    }
+}
